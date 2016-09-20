09cdeca60d65d262a8278dfb52a3164460579cfd
Darcy Parker <darcyparker@gmail.com>
2016-09-20 10:20:49 -0400

    Multiple line description support

---
+++ test/cases/with-description-newlines.ts
@@ -0,0 +1,31 @@
+export var schema = {
+  "title": "Example Schema",
+  "description": "My cool schema",
+  "type": "object",
+  "properties": {
+    "firstName": {
+      "description": "first name single line description",
+      "type": "string"
+    },
+    "lastName": {
+      "type": "string"
+    },
+    "age": {
+      "description": "Age description with\nmultiple lines",
+      "type": "integer",
+      "minimum": 0
+    }
+  },
+  "required": ["firstName", "lastName"]
+}
+
+export var types = `/** My cool schema */
+export interface ExampleSchema {
+  firstName: string; // first name single line description
+  lastName: string;
+  /** Age description with
+   *  multiple lines
+   */
+  age?: number;
+  [k: string]: any;
+}`
