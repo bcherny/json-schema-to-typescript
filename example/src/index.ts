5a4063fdf0faa67a6b80615653f0cb2b58f153e8
Renato Cavalcanti <renato@strongtyped.io>
2016-10-11 15:04:07 +0200

    using a more sensitive model: Person instead of dummy bar, baz and foo

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,8 +2,7 @@ import * as fs from 'fs'
 import { compileFromFile } from 'json-schema-to-typescript'
 
 async function generate() {
-  fs.writeFileSync('dist/model.d.ts', await compileFromFile('src/foo.json'))
-  fs.writeFileSync('dist/model.d.ts', await compileFromFile('src/baz.json'))
+  fs.writeFileSync('dist/person.d.ts', await compileFromFile('src/person.json'))
 }
 
 generate()
