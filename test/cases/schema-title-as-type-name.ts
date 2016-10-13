c96e2162a152858cf9ffa4032430b765eb9c15d2
Renato Cavalcanti <renato@strongtyped.io>
2016-10-13 20:29:03 +0200

    Favor schema.title over schema.id (#54)
    * favors schema.title instead of favor.id (fixes #52)
    
    * removes ReferencedTypeWithId and align with ref.js test

---
+++ test/cases/schema-title-as-type-name.ts
@@ -0,0 +1,48 @@
+export var schema = {
+    "$schema": "http://json-schema.org/draft-03/schema",
+    "id": "http://mycompany.com/api/referencing.json",
+    "title": "Referencing",
+    "type": "object",
+    "properties": {
+        "ref": {
+            "$ref": "../../test/resources/ReferencedType.json"
+        }
+    },
+    "required": ["ref"],
+    "additionalProperties": false
+}
+
+/**
+ * Verify that both generated types names are derived from the schema.title
+ */
+export var configurations = [
+  {
+    settings: {
+      declareReferenced: true
+    },
+    types: `export interface ExampleSchema {
+  firstName: string;
+  lastName: string;
+  /**
+   * Age in years
+   */
+  age?: number;
+  height?: number;
+  favoriteFoods?: any[];
+  likesDogs?: boolean;
+  [k: string]: any;
+}
+export interface Referencing {
+  ref: ExampleSchema;
+}`
+  },
+  {
+    settings: {
+      declareReferenced: false
+    },
+    types: `export interface Referencing {
+  ref: ExampleSchema;
+}`
+  }
+]
+
