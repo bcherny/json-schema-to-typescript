c41f121166f4291e0719b685c70f58b910038c3f
Stefano Dalpiaz <sdalpiaz@atlassian.com>
2018-01-11 17:34:40 +1100

    add partial support for patternProperties

---
+++ test/e2e/patternProperties.4.ts
@@ -0,0 +1,18 @@
+export let input = {
+  id: 'Parent',
+  type: 'object',
+  patternProperties: {
+    '^[a-zA-Z]+': {
+      id: 'Child',
+      type: 'object',
+      properties: {
+        aProperty: { type: 'string' }
+      }
+    }
+  },
+  properties: {
+    a: { type: 'number' },
+    b: { type: 'string' }
+  },
+  required: ['a', 'b']
+}
---
+++ test/e2e/patternProperties.6.ts
@@ -0,0 +1,14 @@
+export let input = {
+  id: 'Parent',
+  type: 'object',
+  patternProperties: {
+    '^[a-zA-Z]+': {
+      id: 'Child',
+      type: 'object',
+      properties: {
+        aProperty: { type: 'string' }
+      }
+    }
+  },
+  additionalProperties: false
+}
