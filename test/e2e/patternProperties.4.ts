fc8540facb7393d86b8f491ba05a2641a6f01a0d
Josh Yudaken <josh@smyte.com>
2018-04-05 15:06:54 -0700

    Implement partial support for patternProperties

---
+++ test/e2e/patternProperties.4.ts
@@ -0,0 +1,15 @@
+export let input = {
+  id: 'Parent',
+  type: 'object',
+  additionalProperties: false,
+  patternProperties: {
+    '^[0-9]+': {
+      id: 'NumberChild',
+      type: 'number'
+    },
+    '^[a-zA-Z]+': {
+      id: 'StringChild',
+      type: 'string'
+    }
+  }
+}
