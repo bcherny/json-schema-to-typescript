b7737b7cf987663b3b73c45a7baa0c3fc25517fa
Sebastian Henke <s.henke@henke-informatik.de>
2018-09-19 06:12:42 +0200

    Fixed TODO for non-destructive CAPS type name handling (#163)
    * Fixed TODO for non-destructive CAPS type name handling
    
    As this commit changes expected output, this commit should be considered to be breaking.
    
    Fixes #159
    
    * Add e2e test for safe type names
    
    * Add more e2e test for safe type names
    
    * Allow type names to contain dollar and underscore
    
    * Update ava e2e snapshot

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { whiteBright } from 'cli-color'
-import { camelCase, isPlainObject, mapValues, upperFirst } from 'lodash'
+import { isPlainObject, mapValues, upperFirst, deburr, trim } from 'lodash'
 import { basename, extname } from 'path'
 
 // TODO: pull out into a separate package
@@ -49,11 +49,25 @@ export function stripExtension(filename: string): string {
 /**
  * Convert a string that might contain spaces or special characters to one that
  * can safely be used as a TypeScript interface or enum name.
- *
- * TODO: be non-destructive for caps (eg. "fooBAR" is ok, and shouldn't be converted to "fooBar")
  */
 export function toSafeString(string: string) {
-  return upperFirst(camelCase(string))
+  // identifiers in javaScript/ts:
+  // First character: a-zA-Z | _ | $
+  // Rest: a-zA-Z | _ | $ | 0-9
+
+  return upperFirst(
+    // remove accents, umlauts, ... by their basic latin letters
+    deburr(string)
+    // replace chars which are not valid for typescript identifiers with whitespace
+    .replace(/(^\s*[^a-zA-Z_$])|([^a-zA-Z_$\d])/g, ' ')
+    // remove underscores followed by lowercase (convert snake_case)
+    .replace(/_[a-z]/g, match => match.substr(1, match.length).toUpperCase())
+    // uppercase letters after digits, dollars
+    .replace(/([\d$]+[a-zA-Z])/g, match => match.toUpperCase())
+    // uppercase first letter after whitespace
+    .replace(/\s+([a-zA-Z])/g, match => trim(match.toUpperCase()))
+    // remove remaining whitespace
+    .replace(/\s/g, ''));
 }
 
 export function generateName(from: string, usedNames: Set<string>) {
---
+++ test/e2e/safeTypeNames.ts
@@ -0,0 +1,127 @@
+export const input = {
+  definitions: {
+    "stra'nge#name": {
+      properties: {
+        "a": { "type": "string" },
+        "b": { "type": "integer" }
+      },
+      additionalProperties: false,
+      required: ["a"]
+    },
+    keepUPPERCASE: {
+      properties: {
+        a: { "enum": ["a", "b", "c"] },
+      }
+    },
+    snake_case: {
+      properties: {
+        "a": { "type": "boolean" },
+      }
+    },
+    camelCase: {
+      properties: {
+        "a": { "type": "float" },
+      }
+    },
+    "kebab-case": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    " startsWithSpace": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "contains space": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "5tartsWithDigit": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    " 5tartsWithBlankAndDigit": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "endsWithDigi7": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "contains4digit": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    ".startsWithPeriod": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "endsWithPeriod.": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "contains...period": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    ",startsWithComma": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "endsWithComma,": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "contains,,Comma": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "$startsWithDollar": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "endsWithDollar$": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "contains$Dollar": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "$": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "UPPERCASE": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+    "Startsuppercase": {
+      properties: {
+        "a": { "type": "string" },
+      }
+    },
+  }
+}
+
+export const options = {
+  unreachableDefinitions: true
+}
+
