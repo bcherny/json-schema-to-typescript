32ae44da71623b0dcc12d1f834e9e94beb14942a
Dillon Williams <dillonwilliams@fastmail.com>
2018-10-15 15:11:34 -0500

    Fix #165 by checking ast names, declare ext option

---
+++ test/e2e/ref.6a.ts
@@ -0,0 +1,15 @@
+export const input = {
+  title: 'Referencing Combined',
+  type: 'object',
+  properties: {
+    foo: {
+      $ref: 'test/resources/ReferencedCombinationType.json'
+    }
+  },
+  required: ['foo'],
+  additionalProperties: false
+}
+
+export const options = {
+  declareExternallyReferenced: true
+}
