40e2b88577d2b0b5fef18c49cbecfe75e73c7f32
赵长伟 <changwei.zcw@alibaba-inc.com>
2020-03-10 11:21:59 +0800

    fix: blank name if all characters filter out

--- src/utils.ts
+++ src/utils.ts
@@ -183,12 +183,15 @@ export function toSafeString(string: string) {
 
 export function generateName(from: string, usedNames: Set<string>) {
   let name = toSafeString(from)
+  if (!name) {
+    name = 'NoName'
+  }
 
   // increment counter until we find a free name
   if (usedNames.has(name)) {
     let counter = 1
     while (usedNames.has(name)) {
-      name = `${toSafeString(from)}${counter}`
+      name = `${name}${counter}`
       counter++
     }
   }
