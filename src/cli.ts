a0257d8421cc6f6fed879c274e7229768dfb2b3b
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-03-14 14:16:37 -0400

    add cli directory support (using globs) (#238)
    * Add directory support
    
    * updates
    
    * reset vscode settings
    
    * fixes on fixes
    
    * complete glob and dir support
    
    * add error check
    
    * remove ?.
    
    * use old node friendly methods
    
    * bump @types/node
    
    * actually add the package.json this time
    
    * resolve pr comments
    
    * add pathTransform test
    
    * fix util test
    
    * remove return and add documentation

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
 
 import {whiteBright} from 'cli-color'
-import {JSONSchema4} from 'json-schema'
 import minimist = require('minimist')
-import {readFile, writeFile} from 'mz/fs'
-import {resolve} from 'path'
+import {readFile, writeFile, existsSync, lstatSync, readdirSync} from 'mz/fs'
+import * as _mkdirp from 'mkdirp'
+import * as _glob from 'glob'
+import isGlob = require('is-glob')
+import {promisify} from 'util'
+import {join, resolve, dirname, basename} from 'path'
 import stdin = require('stdin')
 import {compile, Options} from './index'
+import {pathTransform} from './utils'
+
+// Promisify mkdirp & glob
+const mkdirp = (path: string): Promise<_mkdirp.Made> =>
+  new Promise((res, rej) => {
+    _mkdirp(path, (err, made) => {
+      if (err) rej(err)
+      else res(made === null ? undefined : made)
+    })
+  })
+
+const glob = promisify(_glob)
 
 main(
   minimist(process.argv.slice(2), {
@@ -25,16 +40,96 @@ async function main(argv: minimist.ParsedArgs) {
   }
 
   const argIn: string = argv._[0] || argv.input
-  const argOut: string = argv._[1] || argv.output
+  const argOut: string | undefined = argv._[1] || argv.output // the output can be omitted so this can be undefined
+
+  const ISGLOB = isGlob(argIn)
+  const ISDIR = isDir(argIn)
+
+  if ((ISGLOB || ISDIR) && argOut && argOut.includes('.d.ts')) {
+    throw new ReferenceError(
+      `You have specified a single file ${argOut} output for a multi file input ${argIn}. This feature is not yet supported, refer to issue #272 (https://github.com/bcherny/json-schema-to-typescript/issues/272)`
+    )
+  }
 
   try {
-    const schema: JSONSchema4 = JSON.parse(await readInput(argIn))
-    const ts = await compile(schema, argIn, argv as Partial<Options>)
-    await writeOutput(ts, argOut)
+    // Process input as either glob, directory, or single file
+    if (ISGLOB) {
+      await processGlob(argIn, argOut, argv as Partial<Options>)
+    } else if (ISDIR) {
+      await processDir(argIn, argOut, argv as Partial<Options>)
+    } else {
+      await processFile(argIn, argOut, argv as Partial<Options>)
+    }
   } catch (e) {
     console.error(whiteBright.bgRedBright('error'), e)
     process.exit(1)
   }
+}
+
+// check if path is an existing directory
+function isDir(path: string): boolean {
+  return existsSync(path) && lstatSync(path).isDirectory()
+}
+
+async function processGlob(argIn: string, argOut: string | undefined, argv: Partial<Options>) {
+  const files = await glob(argIn) // execute glob pattern match
+
+  if (files.length === 0) {
+    throw ReferenceError(
+      `You passed a glob pattern "${argIn}", but there are no files that match that pattern in ${process.cwd()}`
+    )
+  }
+  // create output directory if it does not exist
+  if (argOut && !existsSync(argOut)) {
+    await mkdirp(argOut)
+  }
+
+  Promise.all(
+    files.map(file => {
+      const outPath = argOut && `${argOut}/${basename(file, '.json')}.d.ts`
+      processFile(file, outPath, argv)
+    })
+  )
+}
+
+async function processDir(argIn: string, argOut: string | undefined, argv: Partial<Options>) {
+  const files = getPaths(argIn)
+
+  Promise.all(
+    files.map(file => {
+      if (!argOut) {
+        processFile(file, argOut, argv)
+      } else {
+        let outPath = pathTransform(argOut, file)
+        if (!isDir(dirname(outPath))) {
+          _mkdirp.sync(dirname(outPath))
+        }
+        outPath = outPath.replace('.json', '.d.ts')
+        processFile(file, outPath, argv)
+      }
+    })
+  )
+}
+
+async function processFile(argIn: string, argOut: string | undefined, argv: Partial<Options>): Promise<void> {
+  const schema = JSON.parse(await readInput(argIn))
+  const ts = await compile(schema, argIn, argv)
+
+  if (!argOut) {
+    process.stdout.write(ts)
+  } else {
+    return await writeFile(argOut, ts)
+  }
+}
+
+function getPaths(path: string, paths: string[] = []) {
+  if (existsSync(path) && lstatSync(path).isDirectory()) {
+    readdirSync(resolve(path)).forEach(item => getPaths(join(path, item), paths))
+  } else {
+    paths.push(path)
+  }
+
+  return paths
 }
 
 function readInput(argIn?: string) {
@@ -42,18 +137,6 @@ function readInput(argIn?: string) {
     return new Promise(stdin)
   }
   return readFile(resolve(process.cwd(), argIn), 'utf-8')
-}
-
-function writeOutput(ts: string, argOut: string): Promise<void> {
-  if (!argOut) {
-    try {
-      process.stdout.write(ts)
-      return Promise.resolve()
-    } catch (err) {
-      return Promise.reject(err)
-    }
-  }
-  return writeFile(argOut, ts)
 }
 
 function printHelp() {
--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,7 @@ import {run as runCLITests} from './testCLI'
 import {run as runCompileFromFileTests} from './testCompileFromFile'
 import {hasOnly, run as runE2ETests} from './testE2E'
 import {run as runNormalizerTests} from './testNormalizer'
+import {run as runUtilsTests} from './testUtils'
 
 runE2ETests()
 
@@ -9,4 +10,5 @@ if (!hasOnly()) {
   runCompileFromFileTests()
   runCLITests()
   runNormalizerTests()
+  runUtilsTests()
 }
