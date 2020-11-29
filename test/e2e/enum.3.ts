999ac3c47e1976da1a4aa1303be5d2745e912154
Boris Cherny <bcherny@fb.com>
2020-11-28 19:13:42 -0800

    [Test] Add test for #327

---
+++ test/e2e/enum.3.ts
@@ -0,0 +1,18 @@
+export const input = {
+  title: 'InterfaceWithTsEnumNames',
+  anyOf: [{$ref: '#/definitions/InterfaceWithTsEnumNames'}],
+  definitions: {
+    InterfaceWithTsEnumNames: {
+      type: 'object',
+      properties: {
+        TsEnumNames: {$ref: '#/definitions/TsEnums'}
+      },
+      additionalProperties: false
+    },
+    TsEnums: {
+      type: 'string',
+      tsEnumNames: ['publish', 'draft'],
+      enum: ['publish', 'draft']
+    }
+  }
+}
