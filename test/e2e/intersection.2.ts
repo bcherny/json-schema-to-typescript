6dc83ef5c74bf574295115534521221cb85f6001
Boris Cherny <bcherny@fb.com>
2020-11-29 14:02:59 -0800

    [Test] Add test cases for #315

---
+++ test/e2e/intersection.2.ts
@@ -0,0 +1,38 @@
+export const input = {
+  type: 'object',
+  properties: {
+    c: {
+      type: 'string'
+    },
+    d: {
+      type: 'string'
+    }
+  },
+  patternProperties: {
+    '^x-': {}
+  },
+  additionalProperties: false,
+  required: ['c', 'd'],
+  allOf: [
+    {
+      $ref: '#/definitions/A'
+    },
+    {
+      $ref: '#/definitions/B'
+    }
+  ],
+  definitions: {
+    A: {
+      type: 'object',
+      properties: {
+        a: {type: 'string'}
+      }
+    },
+    B: {
+      type: 'object',
+      properties: {
+        b: {type: 'string'}
+      }
+    }
+  }
+}
