f5a4fbd0a3dcb403c9140fa3b08ee6be75d61f64
Boris Cherny <bcherny@fb.com>
2020-12-19 13:38:25 -0800

    [Potential bugfix] Simplify cycle tracking in linker (see #355)

---
+++ test/e2e/refWithCycle.4.ts
@@ -0,0 +1,10 @@
+// @see https://github.com/bcherny/json-schema-to-typescript/issues/355
+const idSchema = {type: 'integer', minimum: 1}
+
+export const input = {
+  type: 'object',
+  properties: {
+    id: idSchema,
+    userId: idSchema
+  }
+}
