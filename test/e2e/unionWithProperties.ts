b3b1d987dd0997c07e58f346dd3fd7a302799e14
Boris Cherny <bcherny@fb.com>
2020-12-19 13:55:44 -0800

    [test] Add test from #328

---
+++ test/e2e/unionWithProperties.ts
@@ -0,0 +1,30 @@
+// @see https://github.com/bcherny/json-schema-to-typescript/pull/328
+export const input = {
+  title: 'UnionWithProps',
+  type: 'object',
+  anyOf: [
+    {
+      type: 'object',
+      required: ['obj_type', 'type'],
+      properties: {
+        obj_type: {type: 'string', enum: ['Foo']},
+        foo_type: {type: 'string'}
+      }
+    },
+    {
+      type: 'object',
+      required: ['health', 'obj_type', 'team', 'type'],
+      properties: {
+        obj_type: {type: 'string', enum: ['Bar']},
+        bar_type: {type: 'string'},
+        team: {type: 'string'},
+        health: {type: 'integer', format: 'uint', minimum: 0.0}
+      }
+    }
+  ],
+  required: ['coords', 'id'],
+  properties: {
+    coords: {type: 'number'},
+    id: {type: 'integer'}
+  }
+}
