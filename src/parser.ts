1e6e41ab3846ac58540654af50ea5caf73bdec6c
Irakli Safareli <i.safareli@gmail.com>
2022-08-22 16:05:42 +0400

    add tsName

--- src/parser.ts
+++ src/parser.ts
@@ -300,7 +300,7 @@ function standaloneName(
   keyNameFromDefinition: string | undefined,
   usedNames: UsedNames
 ): string | undefined {
-  const name = schema.title || schema.$id || keyNameFromDefinition
+  const name = schema.tsName || title || schema.$id || keyNameFromDefinition
   if (name) {
     return generateName(name, usedNames)
   }
