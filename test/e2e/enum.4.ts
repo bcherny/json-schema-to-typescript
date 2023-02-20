cea04aea0ecb1008a9ba51dddb4978d3a7909953
notaphplover <roberto.pintos.lopez@gmail.com>
2023-02-20 13:18:46 +0100

    test: update enum case with additional members

--- test/e2e/enum.4.ts
+++ test/e2e/enum.4.ts
@@ -1,4 +1,4 @@
 export const input = {
   title: 'Enum with JSON literal',
-  enum: [{type: 'string'}]
+  enum: [{type: 'string'}, {foo: 3}, 'foo', {foo: {bar: 'baz'}}, [2, 3, 4], [{foo: [4, 6]}]]
 }
