e2910341ca3c57a6a3e1263ec0c7b673576cf8e6
notaphplover <roberto.pintos.lopez@gmail.com>
2023-02-24 00:06:07 +0100

    fix: update parser to correctly parse boolean json schemas

--- src/parser.ts
+++ src/parser.ts
@@ -17,6 +17,7 @@ import {
 } from './types/AST'
 import {
   getRootSchema,
+  isBoolean,
   isPrimitive,
   JSONSchema as LinkedJSONSchema,
   JSONSchemaWithDefinitions,
@@ -37,6 +38,10 @@ export function parse(
   usedNames = new Set<string>()
 ): AST {
   if (isPrimitive(schema)) {
+    if (isBoolean(schema)) {
+      return parseBooleanSchema(schema, keyName, options)
+    }
+
     return parseLiteral(schema, keyName)
   }
 
@@ -101,6 +106,20 @@ function parseAsTypeWithCache(
   // Update the AST in place. This updates the `processed` cache, as well
   // as any nodes that directly reference the node.
   return Object.assign(ast, parseNonLiteral(schema, type, options, keyName, processed, usedNames))
+}
+
+function parseBooleanSchema(schema: boolean, keyName: string | undefined, options: Options): AST {
+  if (schema) {
+    return {
+      keyName,
+      type: options.unknownAny ? 'UNKNOWN' : 'ANY'
+    }
+  } else {
+    return {
+      keyName,
+      type: 'NEVER'
+    }
+  }
 }
 
 function parseLiteral(schema: JSONSchema4Type, keyName: string | undefined): AST {
