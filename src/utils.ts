15d1e21e7eeb1ec82ef237e0fee4cf27ee9eb251
iansw246 <35947093+iansw246@users.noreply.github.com>
2023-06-30 19:10:53 +0000

    Fix formatting

--- src/utils.ts
+++ src/utils.ts
@@ -166,27 +166,27 @@ export function toSafeString(string: string) {
   // remove accents, umlauts, ... by their basic latin letters
   let safeString = deburr(string)
 
-  let leadingInvalidCharactersMatch = safeString.match(/^\d+[^a-zA-Z_$]*/);
+  const leadingInvalidCharactersMatch = safeString.match(/^\d+[^a-zA-Z_$]*/)
   if (leadingInvalidCharactersMatch?.length !== undefined && leadingInvalidCharactersMatch.length > 0) {
-    let match = leadingInvalidCharactersMatch[0];
-    console.log(match);
-    safeString = safeString.slice(match.length) + match;
-    console.log(safeString);
+    const match = leadingInvalidCharactersMatch[0]
+    console.log(match)
+    safeString = safeString.slice(match.length) + match
+    console.log(safeString)
   }
 
   safeString = safeString
-      // replace chars which are not valid for typescript identifiers with whitespace
-      .replace(/(^\s*[^a-zA-Z_$])|([^a-zA-Z_$\d])/g, ' ')
-      // uppercase leading underscores followed by lowercase
-      .replace(/^_[a-z]/g, match => match.toUpperCase())
-      // remove non-leading underscores followed by lowercase (convert snake_case)
-      .replace(/_[a-z]/g, match => match.substr(1, match.length).toUpperCase())
-      // uppercase letters after digits, dollars
-      .replace(/([\d$]+[a-zA-Z])/g, match => match.toUpperCase())
-      // uppercase first letter after whitespace
-      .replace(/\s+([a-zA-Z])/g, match => trim(match.toUpperCase()))
-      // remove remaining whitespace
-      .replace(/\s/g, '');
+    // replace chars which are not valid for typescript identifiers with whitespace
+    .replace(/(^\s*[^a-zA-Z_$])|([^a-zA-Z_$\d])/g, ' ')
+    // uppercase leading underscores followed by lowercase
+    .replace(/^_[a-z]/g, match => match.toUpperCase())
+    // remove non-leading underscores followed by lowercase (convert snake_case)
+    .replace(/_[a-z]/g, match => match.substr(1, match.length).toUpperCase())
+    // uppercase letters after digits, dollars
+    .replace(/([\d$]+[a-zA-Z])/g, match => match.toUpperCase())
+    // uppercase first letter after whitespace
+    .replace(/\s+([a-zA-Z])/g, match => trim(match.toUpperCase()))
+    // remove remaining whitespace
+    .replace(/\s/g, '')
 
   return upperFirst(safeString)
 }
