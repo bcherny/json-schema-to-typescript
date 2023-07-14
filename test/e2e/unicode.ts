70634220cf455e545122aa956c9d1ffac14b4c4a
iansw246 <35947093+iansw246@users.noreply.github.com>
2023-07-14 21:15:30 +0000

    More Unicode tests

--- test/e2e/unicode.ts
+++ test/e2e/unicode.ts
@@ -5,6 +5,22 @@ export const input = {
     someKey: {
       type: 'string',
       title: '哈哈'
+    },
+    'this is \'I can eat glass in Gothic\' apparently': {
+      type: 'string', 
+      title: '𐌼𐌰𐌲 𐌲𐌻𐌴𐍃 𐌹̈𐍄𐌰𐌽, 𐌽𐌹 𐌼𐌹𐍃 𐍅𐌿 𐌽𐌳𐌰𐌽 𐌱𐍂𐌹𐌲𐌲𐌹𐌸.'
+    },
+    'non ASCII punctuation': {
+      type: 'string',
+      title: '他說：「你好!」',
+    },
+    spanish: {
+      type: 'string',
+      title: 'Utilicé el traductor de google'
+    },
+    myanmar: {
+      type: 'string',
+      title: 'ကျွန်တော် google translate သုံးပါတယ်။'
     }
   }
 }
