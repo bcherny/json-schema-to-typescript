e5950b61b19e02f378f79968d42dc50a6573541d
Travis Chong <78949750+TravisChong@users.noreply.github.com>
2023-07-17 18:05:06 +0000

    Added tests in e2e/unicode.ts for some of the most common langages with unicode characters, temporarily commented out gothic due to unresolved compatibility issue, updated snapshots

--- test/e2e/unicode.ts
+++ test/e2e/unicode.ts
@@ -2,13 +2,13 @@ export const input = {
   type: 'object',
   title: '呵呵',
   properties: {
-    someKey: {
+    chinese: {
       type: 'string',
       title: '哈哈'
     },
     'this is \'I can eat glass in Gothic\' apparently': {
       type: 'string', 
-      title: '𐌼𐌰𐌲 𐌲𐌻𐌴𐍃 𐌹̈𐍄𐌰𐌽, 𐌽𐌹 𐌼𐌹𐍃 𐍅𐌿 𐌽𐌳𐌰𐌽 𐌱𐍂𐌹𐌲𐌲𐌹𐌸.'
+      //title: '𐌼𐌰𐌲 𐌲𐌻𐌴𐍃 𐌹̈𐍄𐌰𐌽, 𐌽𐌹 𐌼𐌹𐍃 𐍅𐌿 𐌽𐌳𐌰𐌽 𐌱𐍂𐌹𐌲𐌲𐌹𐌸.' //Work-in progress (not supported in ES5)
     },
     'non ASCII punctuation': {
       type: 'string',
@@ -21,6 +21,74 @@ export const input = {
     myanmar: {
       type: 'string',
       title: 'ကျွန်တော် google translate သုံးပါတယ်။'
+    },
+    german: {
+      type:'string',
+      title: 'ÄÖÉÜß'
+    },
+    korean: {
+      type: 'string',
+      title: '나는 구글 번역을 사용했다'
+    },
+    japanese: {
+      type: 'string',
+      title: '私はGoogle翻訳を使用しました'
+    },
+    hebrew: {
+      type: 'string',
+      title: 'השתמשתי בגוגל תרגום'
+    },
+    greek: {
+      type: 'string',
+      title: 'Χρησιμοποίησα το Google Translate'
+    },
+    armenian: {
+      type: 'string',
+      title: 'Ես օգտագործել եմ Google translate-ը'
+    },
+    arabic: {
+      type: 'string',
+      title: 'لقد استخدمت مترجم جوجل'
+    },
+    thai: {
+      type: 'string',
+      title: 'ผมใช้ Google แปล'
+    },
+    'Russian/Cyrillic': {
+      type: 'string',
+      title: 'Я использовал Translate.com'
+    },
+    bengali: {
+      type: 'string',
+      title: 'আমি Translate.com ব্যবহার করেছি'
+    },
+    malayalam: {
+      type: 'string',
+      title: 'ഞാൻ ഗൂഗിൾ വിവർത്തനം ഉപയോഗിച്ചു'
+    },
+    georgian: {
+      type: 'string',
+      title: 'Მე გამოვიყენე გუგლის თარგმანი'
+    },
+    tibetian: {
+      type: 'string',
+      title: 'ངས་སྔོན་ཆད་stars21.com'
+    },
+    lao: {
+      type: 'string',
+      title: 'ຂ້ອຍໃຊ້ Google ແປພາສາ'
+    },
+    kmer: {
+      type: 'string',
+      title: 'ខ្ញុំបានប្រើ Google បកប្រែ'
+    },
+    mongolian: {
+      type: 'string',
+      title: 'Би Google Translate ашигласан'
+    },
+    hindi: {
+      type: 'string',
+      title: 'Би Google Translate ашигласан'
     }
   }
 }
