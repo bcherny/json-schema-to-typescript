6ced819ab5de51826c534000ca95d8dbd9db5a26
Travis Chong <78949750+TravisChong@users.noreply.github.com>
2023-07-21 21:13:51 +0000

    extended e2e tet to cover deprecated schema and deprecated:false

--- test/e2e/deprecated.ts
+++ test/e2e/deprecated.ts
@@ -2,10 +2,15 @@ export const input = {
     "$schema": "http://json-schema.org/draft/2019-09/schema#",
     "title": "Example Schema",
     "type": "object",
+    "deprecated": true,
     "properties": {
       "firstName": {
         "type": "string",
         "deprecated": true
+      },
+      "lastName":{
+        "type": "string",
+        "deprecated": false
       },
     },
     "additionalProperties": false,
