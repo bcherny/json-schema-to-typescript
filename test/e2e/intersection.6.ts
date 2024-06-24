1252285c9f934aff7e1875965e779866bab228c4
Boris Cherny <boris@performancejs.com>
2024-06-24 10:41:23 +0200

    add test cases

---
+++ test/e2e/intersection.6.ts
@@ -0,0 +1,36 @@
+export const input = {
+  oneOf: [{$ref: '#/definitions/Car'}, {$ref: '#/definitions/Truck'}],
+  definitions: {
+    Thing: {
+      type: 'object',
+      properties: {
+        name: {type: 'string'},
+      },
+      required: ['name'],
+    },
+    Vehicle: {
+      type: 'object',
+      allOf: [{$ref: '#/definitions/Thing'}],
+      properties: {
+        year: {type: 'integer'},
+      },
+      required: ['year'],
+    },
+    Car: {
+      type: 'object',
+      allOf: [{$ref: '#/definitions/Vehicle'}],
+      properties: {
+        numDoors: {type: 'integer'},
+      },
+      required: ['numDoors'],
+    },
+    Truck: {
+      type: 'object',
+      allOf: [{$ref: '#/definitions/Vehicle'}],
+      properties: {
+        numAxles: {type: 'integer'},
+      },
+      required: ['numAxles'],
+    },
+  },
+}
