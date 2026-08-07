6a4207f45c9d87d7a3fbfaae1695cbbda161a0b0
Mustafa Senoglu <mmustafasenoglu0@gmail.com>
2026-08-07 18:13:06 +0300

    Add noExports option to omit export keyword from generated types
    Closes #650
    
    When generating .d.ts files for plain JavaScript projects (without a
    bundler or transpiler), exported types don't provide IDE hints. This
    adds a noExports option that omits the export keyword, producing
    ambient declarations that IDEs can pick up automatically.
    
    Changes:
    - src/index.ts: Added noExports option to Options interface
    - src/generator.ts: Conditionally omit export in standalone generators
    - src/cli.ts: Added --noExports flag
    - test/e2e/noExports.ts: Added e2e test case

---
+++ test/e2e/noExports.ts
@@ -0,0 +1,17 @@
+export const input = {
+  title: 'No Exports',
+  type: 'object',
+  properties: {
+    firstName: {
+      type: 'string',
+    },
+    age: {
+      type: 'integer',
+    },
+  },
+  required: ['firstName'],
+}
+
+export const options = {
+  noExports: true,
+}
